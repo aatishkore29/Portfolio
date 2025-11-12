1477d213eb78b271f6675528548eb136340af89a
Aatish Kore <aatishkore99@gmail.com>
2025-11-12 21:10:08 +0530

    Correcting import statment

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,5 @@
 import ThemeToggle from "../components/ThemeToggle";
-import { StarBackground } from "../components/Starbackground";
+import { StarBackground } from "../components/StarBackground";
 import { Navbar } from "../components/Navbar";
 import { Hero } from "../components/Hero";
 import { About } from "../components/About";
